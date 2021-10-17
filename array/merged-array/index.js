b2ae757fd2ea3dda0607373c58ac7cf998ff486e
Untung <u.untung0029@gmail.com>
2021-10-17 15:50:52 +0700

    Merged sorted array problem

---
+++ array/merged-array/index.js
@@ -0,0 +1,39 @@
+const mergeSortedArrays = (firstArray, secondArray) => {
+  const mergedArray = [];
+  let array1Item = firstArray[0];
+  let array2Item = secondArray[0];
+
+  console.log(array1Item);
+
+  let i = 1;
+  let j = 1;
+
+  // Check Input
+  if (firstArray.length === 0) {
+    return secondArray;
+  }
+
+  if (secondArray.length === 0) {
+    return firstArray;
+  }
+
+  // Why undefined?
+  // ** At some point the element would be undefined because it is out of range **
+  // Undefined means that the array is already finished and we will add the rest to the mergedArray
+
+  while (array1Item !== undefined || array2Item !== undefined) {
+    if (array2Item === undefined || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = firstArray[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = secondArray[j];
+      j++;
+    }
+  }
+
+  return mergedArray;
+};
+
+mergeSortedArrays([0, 3, 4, 31], [0, 3, 5]);
