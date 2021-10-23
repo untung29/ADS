f8a9a2979e507cd17faa304e43d142f1cb424e73
Untung <u.untung0029@gmail.com>
2021-10-23 11:09:23 +0700

    Implementing keys in the hash table

--- hash-table/implementing-hash-table/index.js
+++ hash-table/implementing-hash-table/index.js
@@ -26,6 +26,7 @@ class HashTable {
     let currentBuckets = this.data[address];
 
     if (currentBuckets.length > 0) {
+      // Because collision might happen!
       for (let i = 0; i < currentBuckets.length; i++) {
         if (currentBuckets[i][0] === key) {
           return currentBuckets[i][1];
@@ -35,6 +36,31 @@ class HashTable {
 
     return undefined;
   }
+
+  keys() {
+    const keysArray = [];
+    // for (let i = 0; i < this.data.length; i++) {
+    //   // It is not an empty memory cell
+    //   if (this.data[i] && this.data[i].length) {
+    //     // also loop through all the potential collisic
+    //     if (this.data.length > 1) {
+    //       for (let j = 0; j < this.data[i].length; j++) {
+    //         keysArray.push(this.data[i][j][0]);
+    //       }
+    //     } else {
+    //       keysArray.push[this.data[i][0]];
+    //     }
+    //   }
+    // }
+
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+
+    return keysArray;
+  }
 }
 
 const myHashTable = new HashTable(50);
@@ -42,3 +68,4 @@ myHashTable.set("grapes", 10000);
 myHashTable.get("grapes");
 myHashTable.set("apples", 9);
 myHashTable.get("apples");
+console.log(myHashTable.keys());
