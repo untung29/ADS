944e308a8135057b5e02e6d746dc42d056606700
Untung <u.untung0029@gmail.com>
2021-11-07 16:17:59 +0700

    Stack implementation array and linked list

--- stack/implementation-array/index.js
+++ stack/implementation-array/index.js
@@ -1,48 +1,20 @@
-// The next value of top is actually the previous value of the top that is implemented in Insert
-
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
 class Stack {
   constructor() {
-    this.top = null;
-    this.bottom = null;
+    this.data = [];
     this.length = 0;
   }
 
   peek() {
-    return this.top;
+    return this.data[this.length - 1];
   }
   push(value) {
-    const newNode = new Node(value);
-    if (this.bottom === null) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      const holdingPointer = this.top;
-      this.top = newNode;
-      this.top.next = holdingPointer;
-    }
-
+    this.data.push(value);
     this.length++;
   }
 
   pop() {
-    if (!this.top) {
-      return null;
-    }
-
-    if (this.top === this.bottom) {
-      this.bottom = null;
-    }
-
-    this.top = this.top.next;
+    this.data.pop();
     this.length--;
-    return this;
   }
 }
 
