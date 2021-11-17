85161e9b09e9f33333cf869a74ba92d46c955d25
Untung <u.untung0029@gmail.com>
2021-11-17 14:32:23 +0700

    Make the bubble sort algorithm to be more efficient

--- sorting/bubble-sort/index.js
+++ sorting/bubble-sort/index.js
@@ -8,15 +8,25 @@
 
 function bubbleSort(array) {
   let left = 0;
+  let counter = 0;
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (array[j] > array[j + 1]) {
-        left = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = left;
+    let swapped = true;
+    if (swapped) {
+      counter++;
+      for (let j = 0; j < array.length; j++) {
+        if (array[j] > array[j + 1]) {
+          left = array[j + 1];
+          array[j + 1] = array[j];
+          array[j] = left;
+          swapped = true;
+        } else {
+          swapped = false;
+        }
       }
     }
   }
+
+  return array;
 }
 
 bubbleSort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]);
